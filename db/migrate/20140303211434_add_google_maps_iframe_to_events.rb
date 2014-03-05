1800375ba725a3cc2df2d1887598053d74b81576
trevmckendrick <trevormckendrick@gmail.com>
2014-03-05 04:05:26 -0700

    update google iframe schema

--- db/migrate/20140303211434_add_google_maps_iframe_to_events.rb
+++ db/migrate/20140303211434_add_google_maps_iframe_to_events.rb
@@ -1,5 +1,5 @@
 class AddGoogleMapsIframeToEvents < ActiveRecord::Migration
   def change
-    add_column :events, :google_maps_iframe, :text
+    add_column :events, :google_maps_iframe, :text, :limit => nil
   end
 end
