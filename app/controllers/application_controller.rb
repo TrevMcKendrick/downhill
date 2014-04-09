9a584be84f1278ef4c8c9a0f5bd59290f778bef8
trevmckendrick <trevormckendrick@gmail.com>
2014-04-09 16:10:58 -0600

    change order of protect_from_forgery callback

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   # prepend_before_action :require_no_authentication, :only => [:after_sign_in_path_for]
-  before_action :configure_devise_permitted_parameters, if: :devise_controller?
-  before_action :check_subdomain
-
   protect_from_forgery with: :exception
 
+  before_action :configure_devise_permitted_parameters, if: :devise_controller?
+  before_action :check_subdomain
 
   protected
 
