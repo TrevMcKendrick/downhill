2cbbf905f93743b826b5b4da41c7a81888a8e1d1
trevmckendrick <trevormckendrick@gmail.com>
2014-04-10 00:01:03 -0600

    move checkout fields to helpers

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -21,6 +21,51 @@ module OrdersHelper
     else
       field = text_field_tag "#{ticket}[]" + 'name[]', nil, :class => "form-control"
     end
-    field.html_safe
+     field.html_safe
+  end
+
+  def email_field(ticket)
+    if Rails.env.development?
+      field = email_field_tag "#{ticket}[]" + 'email[]', nil, :class => "form-control ", :value => "#{rand}@gmail.com"
+    else
+      field = email_field_tag "#{ticket}[]" + 'email[]', nil, :class => "form-control "
+    end
+      field.html_safe
+  end
+
+  def phone_field(ticket)
+    if Rails.env.development?
+      field = phone_field_tag "#{ticket}[]" + 'phone[]', nil, :class => "form-control", :value => "6262444636"
+    else
+      field = phone_field_tag "#{ticket}[]" + 'phone[]', nil, :class => "form-control"
+    end
+      field.html_safe
+  end
+
+  def password_field(ticket)
+    if Rails.env.development?
+      field = text_field_tag "#{ticket}[]" + 'password[]', nil, :class => "form-control", :value => "password_test"
+    else
+      field = text_field_tag "#{ticket}[]" + 'password[]', nil, :class => "form-control"
+    end
+      field.html_safe
+  end
+
+  def card_number_field
+    if Rails.env.development?
+      field = text_field_tag :card_number, nil, :class => "valid payment-form", name: nil, :placeholder => "Card Number", :data => {:stripe => 'number' }, :tabindex => 1, :autofocus => true, :class => "form-control", :value => "4242424242424242"
+    else
+      field = text_field_tag :card_number, nil, :class => "valid payment-form", name: nil, :placeholder => "Card Number", :data => {:stripe => 'number' }, :tabindex => 1, :autofocus => true, :class => "form-control"
+    end
+      field.html_safe
+  end
+
+  def card_code_field
+    if Rails.env.development?
+      field = text_field_tag :card_code, nil,:class => "valid payment-form" , name: nil, :placeholder => "3 or 4 digits", :data => {:stripe => 'cvc' }, :tabindex => 2, :class => "form-control", :value => "4242"
+    else
+      field = text_field_tag :card_code, nil,:class => "valid payment-form" , name: nil, :placeholder => "3 or 4 digits", :data => {:stripe => 'cvc' }, :tabindex => 2, :class => "form-control"
+    end
+      field.html_safe
   end
 end
