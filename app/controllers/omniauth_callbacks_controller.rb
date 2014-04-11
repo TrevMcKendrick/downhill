a14cddeddd14c1765b8a35d74563439d21f57d2e
trevmckendrick <trevormckendrick@gmail.com>
2014-04-11 02:34:25 -0600

    update Stripe API keys + add logging to omniauth callback controller

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,16 +1,20 @@
 class OmniauthCallbacksController < ApplicationController
 
   def authorize_stripe
+    logger.debug "authorize_stripe called"
     code = params[:code]
     user = ActiveSupport::JSON.decode(`curl -X POST https://connect.stripe.com/oauth/token -d client_secret=#{Stripe.api_key} -d code=#{code} -d grant_type=authorization_code`)
+    binding.pry
     set_user_stripe_attributes(user)
 
     redirect_to subdomain_with_https_or_http(DOMAIN_NAME, current_user.subdomain, settings_path)
+    logger.debug "redirected to #{subdomain_with_https_or_http(DOMAIN_NAME, current_user.subdomain, settings_path)}"
   end
 
   private
 
   def set_user_stripe_attributes(user_attributes_hash)
+    logger.debug "set_user_stripe_attributes called. user_attributes_hash.stripe_user_id = #{user_attributes_hash["stripe_user_id"]}"
     current_user.stripe_user_id = user_attributes_hash["stripe_user_id"]
     current_user.stripe_publishable_key = user_attributes_hash["stripe_publishable_key"]
     current_user.stripe_access_token = user_attributes_hash["access_token"]
--- config/initializers/01_constants.rb
+++ config/initializers/01_constants.rb
@@ -1,14 +1,14 @@
 if Rails.env.development? or Rails.env.test?
-  Stripe.api_key = "sk_test_IJphqLl7ZUUxrochhsaJWTJb"
-  STRIPE_PUBLIC_KEY = "pk_test_D8QJoVNlDzFFBB3Mx5xC449q"
+  Stripe.api_key = "sk_test_5kEoB0WcQmNFWgnUVnYX6uAc"
+  STRIPE_PUBLIC_KEY = "pk_test_tl3bInCaozqKo5yJCciHlA2F"
   STRIPE_CLIENT_ID = "ca_3Z5r7GyNyisAgSS23x7STf4gaVYlIQ8Z"
   ROOT_DOMAIN_NAME = "lvh.me"
   DOMAIN_NAME = "#{ROOT_DOMAIN_NAME}:3000"
   COOKIE_DOMAIN = ".lvh.me"
   TICKET_SCIENCE_FEE = 2.95
 elsif Rails.env.production?
-  Stripe.api_key = "sk_live_VhYCleaZwuUBGSFYFc7WUboz"
-  STRIPE_PUBLIC_KEY = "pk_live_8sCPFZoLtW5pXuVHGrRKrROs"
+  Stripe.api_key = "sk_live_EpiTQiYsFlvKQA3T1rs3EiHg"
+  STRIPE_PUBLIC_KEY = "pk_live_Oc672B1KrZ4OWytLidJ1ro94"
   STRIPE_CLIENT_ID = "ca_3Z5rPvHhlZx5VcTBQwIWt1lJahZ69iGH"
   ROOT_DOMAIN_NAME = "ticketscience.com"
   DOMAIN_NAME = ROOT_DOMAIN_NAME
