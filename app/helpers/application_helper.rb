cba9e42fbaeeeaae9a152a8b3f05b54af9f5fdc7
trevmckendrick <trevormckendrick@gmail.com>
2014-04-27 20:45:08 -0600

    Can't publish event unless Stripe is connected

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,8 @@ module ApplicationHelper
       url.split('/').last
   end
 
+  def stripe_set_up?
+    current_user.stripe_user_id != nil && current_user.stripe_user_id != "" ? true : false
+  end
+
 end
--- app/helpers/setting_helper.rb
+++ app/helpers/setting_helper.rb
@@ -1,5 +1,5 @@
 module SettingHelper
-  def stripe_set_up?
-    current_user.stripe_user_id != nil && current_user.stripe_user_id != "" ? true : false
-  end
+  # def stripe_set_up?
+  #   current_user.stripe_user_id != nil && current_user.stripe_user_id != "" ? true : false
+  # end
 end
