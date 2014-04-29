3f476bd0c2063fe087358395a9204c426c131f55
trevmckendrick <trevormckendrick@gmail.com>
2014-04-29 02:03:02 -0600

    Payment form validations

--- app/assets/javascripts/orders.js
+++ app/assets/javascripts/orders.js
@@ -1,6 +1,5 @@
 $(document).ready(function() { 
   // $("#join_team").select2();
-
     $("input[name='team']").change(function(){
       if ($("#team_Join_Team").is(':checked')) 
       {
@@ -14,8 +13,70 @@ $(document).ready(function() {
       else {
         $('#s2id_join_team').hide();
         $('#create_team').show();
+      }  
+    });
+
+  $('#order_form').submit(function(e) {
+      var $form = $(this);
+
+      // Disable the submit button to prevent repeated clicks
+      $form.find('button').prop('disabled', true);
+
+      // $('input').removeClass('invalid');
+      // $('.validation').removeClass('passed failed');
+
+      // var cardType = $.payment.cardType($('.cc-number').val());
+
+      // $('.cc-number').toggleClass('invalid', !$.payment.validateCardNumber($('.cc-number').val()));
+      // $('.cc-exp').toggleClass('invalid', !$.payment.validateCardExpiry($('.cc-exp').payment('cardExpiryVal')));
+      // $('.cc-cvc').toggleClass('invalid', !$.payment.validateCardCVC($('.cc-cvc').val(), cardType));
+
+      // if ( $('input.invalid').length ) {
+      //   $('.validation').addClass('failed');
+      // } else {
+      //   $('.validation').addClass('passed');
+      // }
+
+      var ccvalid = $.payment.validateCardNumber($('#card_number').val());
+      var CVCvalid = $.payment.validateCardCVC($('#card_code').val());
+
+      if ( !ccvalid ) {
+        alert('Your card number isn\'t right, try again!');
+        return false;
       }
-     
+
+      if ( !CVCvalid ) {
+        alert('Your CVC code isn\'t right, try again!');
+        return false;
+      }
+
+      Stripe.createToken($form, stripeResponseHandler);
+
+      // Prevent the form from submitting with the default action
+      return false;
+    });
+
+  $(".payment-form").keyup(function() {
+    $('#card_number').payment('restrictNumeric');
+    $('#card_number').payment('formatCardNumber');
+    $('#card_code').payment('restrictNumeric');
+    $('#card_code').payment('formatCardCVC');
   });
 
 });
+
+   var stripeResponseHandler = function(status, response) {
+      var $form = $('#order_form');
+        if (response.error) {
+        // Show the errors on the form
+        $form.find('.payment-errors').text(response.error.message);
+        $form.find('button').prop('disabled', false);
+          } else {
+        // token contains id, last4, and card type
+        var token = response.id;
+        // Insert the token into the form so it gets submitted to the server
+        $form.append($('<input type="hidden" name="stripeToken" />').val(token));
+        // and submit
+        $form.get(0).submit();
+      }
+    };
--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -38,18 +38,18 @@ module OrdersHelper
 
   def card_number_field
     if Rails.env.development?
-      field = text_field_tag :card_number, nil, :class => "valid payment-form", name: nil, :placeholder => "Card Number", :data => {:stripe => 'number' }, :tabindex => 1, :autofocus => true, :class => "form-control", :value => "4242424242424242"
+      field = text_field_tag :card_number, nil, :class => "valid payment-form form-control", name: nil, :placeholder => "Card Number", :data => {:stripe => 'number' }, :tabindex => 1, :autofocus => true, :value => "4242424242424242"
     else
-      field = text_field_tag :card_number, nil, :class => "valid payment-form", name: nil, :placeholder => "Card Number", :data => {:stripe => 'number' }, :tabindex => 1, :autofocus => true, :class => "form-control"
+      field = text_field_tag :card_number, nil, :class => "valid payment-form form-control", name: nil, :placeholder => "Card Number", :data => {:stripe => 'number' }, :tabindex => 1, :autofocus => true
     end
       field.html_safe
   end
 
   def card_code_field
     if Rails.env.development?
-      field = text_field_tag :card_code, nil,:class => "valid payment-form" , name: nil, :placeholder => "3 or 4 digits", :data => {:stripe => 'cvc' }, :tabindex => 2, :class => "form-control", :value => "4242"
+      field = text_field_tag :card_code, nil,:class => "valid payment-form form-control" , name: nil, :placeholder => "3 or 4 digits", :data => {:stripe => 'cvc' }, :tabindex => 2, :value => "4242"
     else
-      field = text_field_tag :card_code, nil,:class => "valid payment-form" , name: nil, :placeholder => "3 or 4 digits", :data => {:stripe => 'cvc' }, :tabindex => 2, :class => "form-control"
+      field = text_field_tag :card_code, nil,:class => "valid payment-form form-control" , name: nil, :placeholder => "3 or 4 digits", :data => {:stripe => 'cvc' }, :tabindex => 2
     end
       field.html_safe
   end
