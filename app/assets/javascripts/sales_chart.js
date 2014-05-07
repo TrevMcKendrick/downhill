a1802ebbcac93ad0bf5cea1f3556583036b5820f
trevmckendrick <trevormckendrick@gmail.com>
2014-05-06 22:43:23 -0600

    format x axis dates on sales dashboard

--- app/assets/javascripts/sales_chart.js
+++ app/assets/javascripts/sales_chart.js
@@ -10,5 +10,15 @@
   ykeys: ['amount'],
   // Labels for the ykeys -- will be displayed when you hover over the
   // chart.
-  labels: ['Revenue']
+  labels: ['Revenue'],
+
+  xLabelFormat: function (date) {
+  var m_names = new Array("January", "February", "March", 
+"April", "May", "June", "July", "August", "September", 
+"October", "November", "December"); 
+    var curr_date = date.getDate();
+    var curr_month = date.getMonth();
+    var curr_year = date.getFullYear();
+    return m_names[curr_month] + " " + curr_date + ", " + curr_year; 
+  }
 });
