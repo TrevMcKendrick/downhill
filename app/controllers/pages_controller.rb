aa49c9ac5792b3a5f1ee9aa2091695a806720967
trevmckendrick <trevormckendrick@gmail.com>
2014-05-21 14:17:04 -0600

    redirect to dashboard from homepage if logged in

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,6 +4,9 @@ class PagesController < ApplicationController
 
   def home
     cookies[:_cookietest_session] = {domain: COOKIE_DOMAIN}
+    if current_user.present?
+      redirect_to dashboard_url
+    end
   end
 
   def faq
